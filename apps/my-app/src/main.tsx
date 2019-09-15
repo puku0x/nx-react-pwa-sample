3aa5f973daafe0a58b5f0a4ff1019d0d4973c562
puku0x <puku0x@gmail.com>
2019-09-15 10:41:27 +0900

    add pwa capability

--- apps/my-app/src/main.tsx
+++ apps/my-app/src/main.tsx
@@ -3,4 +3,16 @@ import ReactDOM from 'react-dom';
 
 import App from './app/app';
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
